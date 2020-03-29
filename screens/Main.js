a146222fbd318cfc1942211d82bf2fb31c39a3bb
Rapha-Y <raph.yuki@gmail.com>
2020-03-29 16:16:05 -0300

    added direct passage of screen component props

--- screens/Main.js
+++ screens/Main.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import User from './User';
+
+function firstTab() {
+    return(
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>First</Text>
+        </View>
+    );
+}
+
+function secondTab() {
+    return(
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>Second</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function Main({ route }) {
     const { id } = route.params;
     return(
-        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Text>{id}</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="First" component={firstTab} />
+            <Tab.Screen name="Second" component={secondTab} />
+            <Tab.Screen name="User">
+                {props => <User {...props} id={id} />}
+            </Tab.Screen>
+        </Tab.Navigator>
     );
 }
